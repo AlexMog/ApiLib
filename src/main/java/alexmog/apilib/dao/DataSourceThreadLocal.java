5938d831714b76d4eb313a0c1bb254107350e7f9
AlexMog <alexmog@live.fr>
2018-11-22 00:03:20 +0100

    Added new DAO systems

---
+++ src/main/java/alexmog/apilib/dao/DataSourceThreadLocal.java
@@ -0,0 +1,49 @@
+package alexmog.apilib.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.function.Supplier;
+
+import com.jolbox.bonecp.BoneCPDataSource;
+
+public class DataSourceThreadLocal {
+	private final BoneCPDataSource mDataSource;
+	private final ThreadLocal<Connection> mThreadLocal;
+	
+	public DataSourceThreadLocal(BoneCPDataSource dataSource) {
+		mDataSource = dataSource;
+		mThreadLocal = new ThreadLocal<>();
+		ThreadLocal.withInitial(new Supplier<Connection>() {
+
+			@Override
+			public Connection get() {
+				return null;
+			}
+		});
+	}
+	
+	public void releaseConnection() throws SQLException {
+		Connection conn = mThreadLocal.get();
+		if (conn == null) return;
+		mThreadLocal.remove();
+		conn.close();
+	}
+	
+	public Connection prepareDataSource() throws SQLException {
+		Connection conn = mThreadLocal.get();
+		if (conn != null) return conn;
+		conn = mDataSource.getConnection();
+		mThreadLocal.set(conn);
+		return conn;
+	}
+	
+	public Connection getConnection() throws SQLException {
+		Connection conn = mThreadLocal.get();
+		if (conn == null) conn = prepareDataSource();
+		return conn;
+	}
+
+	public void close() {
+		mDataSource.close();
+	}
+}
